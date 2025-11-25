447ce808f16bac524eb86985b1efa746aa6ab75f
Tanvir <sabbirtanvirahmed18@gmail.com>
2025-11-26 01:34:38 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = "mongodb+srv://travel_db:KXJENTg9bsmopJRh@cluster0.tx061fa.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db('travel_db');
+    const PackagesCollection = db.collection('Packages');
+
+    console.log("MongoDB connected!");
+
+    app.get('/', (req, res) => {
+      res.send('Hello World!');
+    });
+
+    // all packages
+    app.get('/Packages', async (req, res) => {
+      
+        const result = await PackagesCollection.find().toArray();
+        res.send(result);
+    });
+
+    // Show 6 packages
+    app.get("/latest-Packages", async (req, res) => {
+        const result = await PackagesCollection
+          .find()
+          .sort({ createdAt: -1 }) 
+          .limit(6)
+          .toArray();
+        res.send(result);
+      
+    });
+
+
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+run().catch(console.dir);
