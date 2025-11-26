775b32664ff23d78abcadd92c5473e95f6207f84
Tanvir <sabbirtanvirahmed18@gmail.com>
2025-11-27 00:26:01 +0600

    added packages

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -32,22 +32,53 @@ async function run() {
 
     // all packages
     app.get('/Packages', async (req, res) => {
-      
-        const result = await PackagesCollection.find().toArray();
-        res.send(result);
+
+      const result = await PackagesCollection.find().toArray();
+      res.send(result);
     });
+
+
+
+    // ADD NEW PACKAGE
+    app.post("/Packages", async (req, res) => {
+      const data = req.body;
+
+      // createdAt add
+      data.createdAt = new Date();
+
+      const result = await PackagesCollection.insertOne(data);
+
+      res.send({
+        success: true,
+        message: "Package added successfully!",
+        insertedId: result.insertedId,
+      });
+    });
+
+
+
+
 
     // Show 6 packages
     app.get("/latest-Packages", async (req, res) => {
-        const result = await PackagesCollection
-          .find()
-          .sort({ createdAt: -1 }) 
-          .limit(6)
-          .toArray();
-        res.send(result);
-      
+      const result = await PackagesCollection
+        .find()
+        .sort({ createdAt: -1 })
+        .limit(6)
+        .toArray();
+      res.send(result);
+
     });
 
+
+    app.get("/Packages/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await PackagesCollection.findOne({ _id: new ObjectId(id) });
+      if (!result) {
+        return res.send({ success: false, message: "Package not found" });
+      }
+      res.send({ success: true, result });
+    });
 
     app.listen(port, () => {
       console.log(`Server running on port ${port}`);
